7b055fef3aff3ca75076fd08b86b194cd5192acd
Mikhail Gr <g_ms@bk.ru>
2025-01-02 20:53:50 +0300

    init

---
+++ __tests__/healthStatus.test.js
@@ -0,0 +1,16 @@
+import { healthStatus } from '../src/healthStatus';
+
+test('healthStatus should return "healthy" for health > 50', () => {
+  const result = healthStatus({ name: 'Маг', health: 90 });
+  expect(result).toBe('healthy');
+});
+
+test('healthStatus should return "wounded" for health between 15 and 50', () => {
+  const result = healthStatus({ name: 'Маг', health: 30 });
+  expect(result).toBe('wounded');
+});
+
+test('healthStatus should return "critical" for health < 15', () => {
+  const result = healthStatus({ name: 'Маг', health: 10 });
+  expect(result).toBe('critical');
+});
